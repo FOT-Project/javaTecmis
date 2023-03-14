74d3044e02778ae23b832f1120f26d95620f7b97
Hasitha Sandeepa <hasithasandeepa2000@gmail.com>
2023-03-14 08:05:19 +0530

    Hasitha - Main Admin - 23.03.14 - 08.05.am

--- Tecmis-group01/javaTecmis/src/DBConn/DBconn.java
+++ Tecmis-group01/javaTecmis/src/DBConn/DBconn.java
@@ -1,26 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+
 package DBConn;
 
 import java.sql.*;
 
 public class DBconn {
     
+    public String url = "jdbc:mysql://191.96.56.1:3306/u812963415_javag1";
+    public String user = "u812963415_javag1";
+    public String password = "p*lC5tH0^";
+    public Connection conn;
+    public Statement stm;
+    
     public static void main(String[] args) {
-        connect();
+     
     }
     
-    public static Connection connect()
+    public void connect()
     {
-        Connection conn = null;
-        
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://191.96.56.1:3306/u812963415_javag1", "u812963415_javag1", "p*lC5tH0^");
+            
+            conn = DriverManager.getConnection(url, user, password);
+            
+            stm = conn.createStatement();
             //System.out.println("Connected");
-        } catch (Exception e) {
-            System.out.println("Connection Error: " +e);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Connection Error: " +ex);
         }
-        
-        return conn;
     }
             
 }
