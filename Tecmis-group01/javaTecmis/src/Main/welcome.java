7d5801b8fa56a8aa4107aa0075a784ba16e2b7f0
Hasitha Sandeepa <hasithasandeepa2000@gmail.com>
2023-03-19 13:56:13 +0530

    Hasitha - Admin, Main - 23.03.19 - 01.55pm
    change something in welcome and admin package

--- Tecmis-group01/javaTecmis/src/Main/welcome.java
+++ Tecmis-group01/javaTecmis/src/Main/welcome.java
@@ -134,18 +134,7 @@ public class Welcome extends javax.swing.JFrame {
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
+
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
