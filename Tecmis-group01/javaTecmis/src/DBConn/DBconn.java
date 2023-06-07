823ccb5c6b7af554ff0854c28b8a3babb11dc19b
Hasitha Sandeepa <hasithasandeepa2000@gmail.com>
2023-06-07 18:08:27 +0530

    2023.06.07 - 06.08PM - Upload and download

--- Tecmis-group01/javaTecmis/src/DBConn/DBconn.java
+++ Tecmis-group01/javaTecmis/src/DBConn/DBconn.java
@@ -10,10 +10,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBconn {
-    
-    public static void main(String[] args) {
-        connect();
-    }
+
     
     public static Connection connect()
     {
@@ -21,8 +18,8 @@ public class DBconn {
         
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://191.96.56.1:3306/u812963415_javag1", "u812963415_javag1", "p*lC5tH0^");
-            //System.out.println("Connected");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tecmis", "root", "");
+            System.out.println("Connected");
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println("Connection Error: " +e);
         }
